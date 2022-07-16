5065dd9720a5a9b6ededb7775b0200006dcb9783
Abdul Basit <abdulbasitansari288@gmail.com>
2022-07-16 11:41:25 +0500

    Change Debug Mode

--- myprojects/settings.py
+++ myprojects/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 ALLOWED_HOSTS = ["ab-projects-zone.herokuapp.com","localhost","127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ["https://ab-projects-zone.herokuapp.com"]
