52216a1f7d2be613b799240034a7d7eb507d43d8
Abdul Basit <abdulbasitansari288@gmail.com>
2022-07-19 14:17:36 +0500

    Debug Mode Off

--- myprojects/settings.py
+++ myprojects/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 # ALLOWED_HOSTS = ["ab-projects-zone.herokuapp.com","localhost","127.0.0.1"]
 ALLOWED_HOSTS = ["ab-projects-zone.herokuapp.com","localhost","127.0.0.1"]
 
