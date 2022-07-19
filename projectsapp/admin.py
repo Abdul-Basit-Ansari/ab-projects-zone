a5c65cf0ce117ff377cf099dd9db164a8e79ad46
Abdul Basit <abdulbasitansari288@gmail.com>
2022-07-19 14:19:30 +0500

    Add Skill Model In Admin

--- projectsapp/admin.py
+++ projectsapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Project,Contactus,Feedback
+from . models import Project,Contactus,Feedback,Skill
 # Register your models here.
 
-admin.site.register((Project,Contactus,Feedback))
+admin.site.register((Project,Contactus,Feedback,Skill))
